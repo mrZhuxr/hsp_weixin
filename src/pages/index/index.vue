07f1d2f7b57fab38ace601c6c604af3cfc5d8b92
zhuxiaorong <979757942@qq.com>
2023-10-08 10:06:11 +0800

    修改

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,7 +3,7 @@
         :src="`https://showroom-oss.vitoreality.com/Oasis_HSP/index.html#/login?openid=${loginCode}`"
     ></web-view> -->
     <web-view
-        :src="`https://showroom-oss.vitoreality.com/Oasis_HSP/index.html#/login?openid=${loginCode}&nickName=${nickName}&avatarUrl=${avatarUrl}`"
+        :src="`https://showroom-oss.vitoreality.com/Oasis_HSP/index.html#/login?openid=${loginCode}`"
     ></web-view>
 </template>
 
@@ -11,8 +11,6 @@
 import { ref, onMounted } from 'vue'
 
 const loginCode = ref('')
-const nickName = ref('')
-const avatarUrl = ref('')
 const login_button = ref<HTMLButtonElement | null>(null)
 onMounted(() => {
     uni.login({
@@ -29,11 +27,4 @@ onMounted(() => {
 
     login_button.value?.click()
 })
-
-const getUserProfile = async () => {
-    const { userInfo } = await uni.getUserProfile({ desc: '用于完善个人资料' })
-    console.log('🚀 ~ file: index.vue:39 ~ getUserProfile ~ userInfo:', userInfo)
-    nickName.value = userInfo.nickName
-    avatarUrl.value = userInfo.avatarUrl
-}
 </script>
